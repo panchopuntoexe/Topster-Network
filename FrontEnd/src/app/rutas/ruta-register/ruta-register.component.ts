8215fc4c8ed36e4eba900584fa2079dee734efd2
David <dave_martins@hotmail.com>
2022-03-09 23:17:58 -0500

    Update/ Funcionalidades Inicales Register

--- FrontEnd/src/app/rutas/ruta-register/ruta-register.component.ts
+++ FrontEnd/src/app/rutas/ruta-register/ruta-register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DbtopsterService } from 'src/app/servicios/html/dbtopster.service';
+import { UsuarioInterfaz } from 'src/app/servicios/interfaces/UsuarioInterfaz';
 
 @Component({
   selector: 'app-ruta-register',
@@ -9,32 +11,39 @@ import { DbtopsterService } from 'src/app/servicios/html/dbtopster.service';
 })
 export class RutaRegisterComponent implements OnInit {
 
+  mostrarError = false;
+  formGroup?: FormGroup;
+  usuario?: UsuarioInterfaz;
+
   constructor(
-    private readonly dbTopsterService: DbtopsterService,
+    private readonly dbTopsterService: DbtopsterService,    
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly formBuilder: FormBuilder,
     private readonly router: Router,
   ) { }
 
   ngOnInit(): void {
+    this.prepararFormulario();
   }
 
   crearUsuario() {
-    // const usuarioACrear = this.prepararObjeto();
-    // const actualizar$ = this.dbTopsterService
-    //   .crearUsuario(
-    //     usuarioACrear
-    //   );
-    // actualizar$
-    //   .subscribe({
-    //     next: (datos: any) => {
-    //       console.log(datos)
+    const usuarioACrear = this.prepararObjeto();
+    const actualizar$ = this.dbTopsterService
+      .crearUsuario(
+        usuarioACrear
+      );
+    actualizar$
+      .subscribe({
+        next: (datos: any) => {
+          console.log(datos)
           const ruta = ['/login'];
           this.router.navigate(ruta);
 
-    //     },
-    //     error: (error: any) => {
-    //       console.error({ error })
-    //     }
-    //   });
+        },
+        error: (error: any) => {
+          console.error({ error })
+        }
+      });
   }
 
 
