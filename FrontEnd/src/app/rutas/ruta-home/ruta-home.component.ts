42f64e2c451a246d13bb84e56341a6d2771e8846
David <dave_martins@hotmail.com>
2022-03-11 14:33:54 -0500

    Update/ Funcionalidad Home Incorporada

--- FrontEnd/src/app/rutas/ruta-home/ruta-home.component.ts
+++ FrontEnd/src/app/rutas/ruta-home/ruta-home.component.ts
@@ -71,23 +71,115 @@ export class RutaHomeComponent implements OnInit {
     this.esUsuarioLogueado = (localStorage.getItem('nombreDeUsuario') == this.nombreDeUsuario) ? true : false
     this.obtenerUsuarioLogueado()
     this.obtenerPosts()
+    this.obtenerPostsDeSeguidos()
   }
 
+  obtenerPosts() {
+    //Se cargan los post realizados por el usuario
+    this.dbTopsterService.consultarPostsPorUsuarioId(this.usuario.idUsuario)
+      .subscribe({
+        next: (datos) => {
+          this.postsDeUsuario = Object.assign(this.postsDeUsuario, datos);
+        },
+        error: (error) => {
+          console.error({ error });
+        }
+      })
+  }
 
-  // obtenerPosts() {
-  //   //Se cargan los post de los usuarios a los que sigue 
-  //   for (var i = 0; i < this.arregloSeguidores.length; i++) {
-  //     console.log(i)
-  //     this.dbTopsterService.consultarPostsPorUsuarioId(this.arregloSeguidores[i])
-  //       .subscribe({
-  //         next: (datos) => {
-  //           this.postsDeUsuario = Object.assign(this.postsDeUsuario, datos);
-  //         },
-  //         error: (error) => {
-  //           console.error({ error });
-  //         }
-  //       })
-  //   }
+  obtenerPerfilUsuario() {
+    this.dbTopsterService.consultarUsuariosPorNombre(this.nombreDeUsuario)
+      .subscribe({
+        next: (datos) => {
+          this.usuario = Object.assign({}, datos[0]);
+          this.obtenerPosts()
+        },
+        error: (error) => {
+          console.error({ error });
+        }
+      })
+  }
+
+    // Obteniendo los ids de los seguidores e insertandolos en un arreglo
+    obtenerPostsDeSeguidos(){
+      this.dbTopsterService.consultarUsuariosPorNombre(this.nombreDeUsuario)
+        .subscribe({
+          next: (datos) => {
+            this.idUsuario = datos[0].idUsuario;
+
+            this.dbTopsterService.consultarSeguimientodeUsuarioId(this.idUsuario)
+            .subscribe({
+              next: (datos) => {
+                  datos.forEach((value, index, datos) => {
+                    this.arregloSeguidores.push(datos[index].idUsuarioSeguidor)
+                  })
+
+                  //Problema Aquí
+                  // Estoy iterando entre los seguidos y obteniendo sus postId
+                  // Problema existente porque se generan n número de arreglos según los n seguidos
+                  // cada uno con los postId de cada seguido
+                  // Lo que dificulta iterar una vez más en un arreglo de posts (porque son n) 
+                  this.arregloSeguidores.forEach((value, index, datos) => {
+                    this.dbTopsterService.consultarPostsPorUsuarioId(datos[index].valueOf())
+                      .subscribe({
+                        next: (datos) => {
+                          datos.forEach((value, index, datos) => {
+                            this.arregloPosts.push(datos[index].idPost)
+                          })
+                          this.arregloPosts.forEach((value, index, datos) => {
+                            this.postsDeUsuario = Object.assign(this.postsDeUsuario, datos[index].valueOf());
+                          })
+                        },
+                        error: (error) => {
+                          console.error({ error });
+                        }
+                      })
+                  })
+
+                this.estaSiguiendo ? this.textoDeSeguir = "Dejar de seguir" : this.textoDeSeguir = "Seguir"
+              },
+              error: (error) => {
+                console.error({ error });
+              }
+            })
+
+          },
+          error: (error) => {
+            console.error({ error });
+          },
+        })
+    }
+
+
+  // obtenerPostsDeSeguidos() {
+  //   this.arregloSeguidores = this.obtenerSeguidosId();
+  //   console.log(this.obtenerSeguidosId())
+  //   console.log(this.arregloSeguidores)
+  //   // for (var i = 0; i < this.arregloSeguidores.length; i++) {
+  //   //   this.dbTopsterService.consultarPostsPorUsuarioId(Object.values(this.arregloSeguidores))
+  //   //     .subscribe({
+  //   //       next: (datos) => {
+  //   //         this.postsDeUsuario = Object.assign(this.postsDeUsuario, datos);
+  //   //       },
+  //   //       error: (error) => {
+  //   //         console.error({ error });
+  //   //       }
+  //   //     })
+  //   // }
+
+  //   // //Se cargan los post de los usuarios a los que sigue 
+  //   // for (var i = 0; i < this.arregloSeguidores.length; i++) {
+  //   //   console.log(i)
+  //   //   this.dbTopsterService.consultarPostsPorUsuarioId(this.arregloSeguidores[i])
+  //   //     .subscribe({
+  //   //       next: (datos) => {
+  //   //         this.postsDeUsuario = Object.assign(this.postsDeUsuario, datos);
+  //   //       },
+  //   //       error: (error) => {
+  //   //         console.error({ error });
+  //   //       }
+  //   //     })
+  //   // }
 
   //   // this.arregloSeguidores.forEach((value, index, datos) =>{
   //   //   this.dbTopsterService.consultarPostsPorUsuarioId(datos[index].valueOf())
@@ -101,69 +193,34 @@ export class RutaHomeComponent implements OnInit {
   //   //   })
   //   // })
   // }
+  
+  // obtenerSeguidosId() : any {
+  //   this.dbTopsterService.consultarUsuariosPorNombre(this.nombreDeUsuario)
+  //     .subscribe({
+  //       next: (datos) => {
+  //         this.idUsuario = datos[0].idUsuario;
+  //         this.dbTopsterService.consultarSeguimientodeUsuarioId(this.idUsuario)
+  //           .subscribe({
+  //             next: (datos) => {
+  //               if (datos != null) {
+  //                 datos.forEach((value, index, datos) => {
+  //                   this.arregloSeguidores.push(datos[index].idUsuarioSeguidor)
+  //                 })
+  //               }
+  //               this.estaSiguiendo ? this.textoDeSeguir = "Dejar de seguir" : this.textoDeSeguir = "Seguir"
+  //             },
+  //             error: (error) => {
+  //               console.error({ error });
+  //             }
+  //           })
+  //       },
+  //       error: (error) => {
+  //         console.error({ error });
+  //       }
+  //     })
+  //   return [...this.arregloSeguidores]
+  // }
 
-  obtenerPerfilUsuario() {
-    this.dbTopsterService.consultarUsuariosPorNombre(this.nombreDeUsuario)
-      .subscribe({
-        next: (datos) => {
-          this.usuario = Object.assign({}, datos[0]);
-        },
-        error: (error) => {
-          console.error({ error });
-        }
-      })
-  }
-
-  // Obteniendo los ids de los seguidores e insertandolos en un arreglo
-  obtenerPosts(){
-    this.dbTopsterService.consultarUsuariosPorNombre(this.nombreDeUsuario)
-      .subscribe({
-        next: (datos) => {
-          this.idUsuario = datos[0].idUsuario;
-          this.dbTopsterService.consultarSeguimientodeUsuarioId(this.idUsuario)
-            .subscribe({
-              next: (datos) => {
-                if (datos != null) {
-                  datos.forEach((value, index, datos) => {
-                    this.arregloSeguidores.push(datos[index].idUsuarioSeguidor)
-                  })
-                  console.log(this.arregloSeguidores.length)
-                  //Problema Aquí
-                  // Estoy iterando entre los seguidos y obteniendo sus postId
-                  // Problema existente porque se generan n número de arreglos según los n seguidos
-                  // cada uno con los postId de cada seguido
-                  // Lo que dificulta iterar una vez más en un arreglo de posts (porque son n) 
-                  this.arregloSeguidores.forEach((value, index, datos) => {
-                    this.dbTopsterService.consultarPostsPorUsuarioId(datos[index].valueOf())
-                      .subscribe({
-                        next: (datos) => {
-                          console.log(datos)
-                          datos.forEach((value, index, datos) => {
-                            this.arregloPosts.push(datos[index].idPost)
-                          })
-                          // this.arregloPosts.forEach((value, index, datos) => {
-                            this.postsDeUsuario = Object.assign(this.postsDeUsuario, this.arregloPosts);
-                          // })
-                        },
-                        error: (error) => {
-                          console.error({ error });
-                        }
-                      })
-                  })
-                  // Hasta acá
-                }
-                this.estaSiguiendo ? this.textoDeSeguir = "Dejar de seguir" : this.textoDeSeguir = "Seguir"
-              },
-              error: (error) => {
-                console.error({ error });
-              }
-            })
-        },
-        error: (error) => {
-          console.error({ error });
-        }
-      })
-  }
 
   obtenerUsuarioLogueado() {
     var nombreDeUsuarioLogueado: string = ""
@@ -179,30 +236,5 @@ export class RutaHomeComponent implements OnInit {
         }
       })
   }
-
-
-  // obtenerPosts(){
-
-  //   //Problema Aquí
-  //   this.arregloSeguidores.forEach((value, index, datos) => {
-  //     this.dbTopsterService.consultarPostsPorUsuarioId(datos[index].valueOf())
-  //       .subscribe({
-  //         next: (datos) => {
-  //           console.log(datos)
-  //           datos.forEach((value, index, datos) => {
-  //             this.arregloPosts.push(datos[index].idPost)
-  //           })
-  //           this.arregloPosts.forEach((value, index, datos) => {
-  //             this.postsDeUsuario = Object.assign(this.postsDeUsuario, datos.values);
-  //           })
-  //         },
-  //         error: (error) => {
-  //           console.error({ error });
-  //         }
-  //       })
-  //   })
-  //   // Hasta acá
-  // }
-
-
+  
 }
